807913b66b0d915f40bcb28d8cbc1ef179128f3a
MyrPasko <myroslavpasko@gmail.com>
2018-12-09 07:07:32 +0200

    On lesson 78. Chain with external branch 78 on git.

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,3 @@
-const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path'); // The path to main folder of project;
@@ -9,16 +8,6 @@ const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    console.warn('admin.js', "Hello from Express!");
-    // res.send('<h1>Hello from Add-Product Middleware!!!</h1>');
-    // res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit" >Submit</button></form>');
-    // res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
-    /** This is for usual HTML */
-    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-    /** And this is for PUG */
-    /** And this is for Handlebars too */
-    /** 'activeAddProduct', 'formCSS', 'productCSS' are for Handlebars only */
-    /** And this is for EJS too */
     res.render('add-product', {
         pageTitle: 'add-product',
         path: '/admin/add-product',
@@ -33,8 +22,6 @@ router.post('/add-product', (req, res, next) => {
     products.push({title: req.body.title});
     res.redirect('/')
 });
-
-// module.exports = router;
 
 exports.routes = router;
 exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -7,16 +7,8 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    // res.send('<h1>Hello from Express!</h1>');
-    // res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
-    console.log('shop.js', adminData.products);
-    /** This is for usual HTML */
-    // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-    /** And this is for PUG */
-    /** And this is for Handlebars too */
+
     const products = adminData.products;
-    /** 'HasProducts', 'activeShop', 'productCSS' keys is only for Handlebars */
-    /** And this is for EJS too */
     res.render('shop', {
         prods: products,
         pageTitle: 'shop',
