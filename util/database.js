1347a4cd693ecda16d071e24c7142ed3ebf4c0d4
MyrPasko <myroslavpasko@gmail.com>
2019-01-03 06:42:26 +0200

    On lesson 120.

---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node_complete',
+    password: 'Pp><2*1*9--'
+});
+
+module.exports = pool.promise();
