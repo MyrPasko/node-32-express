a03f289639862ec232944afcc3ef6e305956e2ae
MyrPasko <myroslavpasko@gmail.com>
2019-01-16 06:27:39 +0200

    On lesson 153.

--- controllers/shop.js
+++ controllers/shop.js
@@ -123,8 +123,10 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
+    let fetchedCart;
     req.user.getCart()
         .then((cart) => {
+            fetchedCart = cart;
             return cart.getProducts();
         })
         .then((products) => {
@@ -137,21 +139,30 @@ exports.postOrder = (req, res, next) => {
                         return product;
                     }))
                 })
-                .then((result) => {
-
-                })
-                .catch((err) => {
-
-                });
+        })
+        .then((result) => {
+            return fetchedCart.setProducts(null);
+        })
+        .then((result) => {
+            res.redirect('/orders')
         })
         .catch((err) => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-    res.render('shop/orders', {
-        path: '/orders',
-        pageTitle: 'Your Orders'
-    });
+    req.user
+        .getOrders({include: ['products']})
+        .then((orders) => {
+            console.log("Orders: ", orders);
+            res.render('shop/orders', {
+                path: '/orders',
+                pageTitle: 'Your Orders',
+                orders: orders,
+            });
+        })
+        .catch((err) => {
+            console.log("Error from getOrders: ", err);
+        });
 };
 
 exports.getCheckout = (req, res, next) => {
